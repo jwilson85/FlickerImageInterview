7d99578e625b8917bb402b090c30158fb32776c3
Jay <jeremy.wilson@remax.com>
2021-11-09 14:34:59 -0700

    Initial Commit

---
+++ FlickerImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickerImages
+//
+//  Created by Wilson, Jeremy on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
